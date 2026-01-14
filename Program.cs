ba6e3c0d966cf17b2e4677dd77cda97c1f978d7f
arroyanaraisyah <arroyana.raisyah@gmail.com>
2026-01-14 14:47:22 +0700

    menambahkanSesuatu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int  c = 0;
+
+void tambah (int a, int b)
+{
+   c = a + b;
+    Console.WriteLine($"{a} + {b} = {c}");
+}
+void kurang  (int a, int b)
+{
+   c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+}
+void kali  (int a, int b)
+{
+   c = a * b;
+    Console.WriteLine($"{a} * {b} = {c}");
+}
+void bagi  (int a, int b)
+{
+   c = a / b;
+    Console.WriteLine($"{a} / {b} = {c}");
+}
+
+tambah(1,2);
+kurang(1,2);
+kali(1,2);
+bagi(1,2);
